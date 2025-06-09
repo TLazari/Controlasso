a148a6e8dd36cf2318bab0fa06834baec7a08d8e
Matheus-Maia <matheusmaiadev535@gmail.com>
2025-06-09 11:08:43 -0300

    fix: Update home view to render the correct home page template path

--- app_controlasso/views.py
+++ app_controlasso/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'app_controlasso/pages/home.html')
+    ...
+    return render(request, 'pages/home.html')
