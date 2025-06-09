4d9f7cc465bba1ddd24d3748c1673cb8469130c9
Matheus-Maia <matheusmaiadev535@gmail.com>
2025-06-09 11:09:07 -0300

    fix: Update URL routing to include app_controlasso at the root path

--- projeto_controlasso/urls.py
+++ projeto_controlasso/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app_controlasso/', include('app_controlasso.urls')),
+    path('', include('app_controlasso.urls')),
 ]
