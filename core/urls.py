a7e3d74701756aaf48886e9bf1fe1ac0c7e2a9be
Matheus <matheusmaiadev535@gmail.com>
2025-06-13 22:00:33 -0300

    Matheus maia/start_app_core (#14)
    * feat: adiciona estrutura de URLs e views para o aplicativo core
    
    * fix: re-atualiza rotas de usuários para incluir login, registro e logout
    
    * feat: adiciona página inicial e rotas correspondentes ao aplicativo core
    
    ---------
    
    Co-authored-by: Matheus Maia <matheusmaia535@gmail.com>

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = "core"
+
+# URLS
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('transferencia/', views.transferencia_view, name='transferencia'),
+    path('transferencias/historico/', views.transferencias_historico_view, name='transferencias_historico'),
+    path('acoes/', views.acoes_list_view, name='acoes_list'),
+    path('acoes/favoritas/', views.acoes_favoritas_view, name='acoes_favoritas'),
+    path('acoes/compradas/', views.acoes_compradas_view, name='acoes_compradas'),
+    path('acoes/<str:codigo>/', views.acao_detail_view, name='acao_detail'),
+    path('perfil/', views.perfil_view, name='perfil'),
+]
---
+++ core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def dashboard_view(request):
+    ...
+    return render(request, "core/pages/dashboard.html")
+
+def transferencia_view(request):
+    ...
+    return render(request, "core/pages/transferencia.html")
+
+def transferencias_historico_view(request):
+    ...
+    return render(request, "core/pages/transferencias_historico.html")
+
+def acoes_list_view(request):
+    ...
+    return render(request, "core/pages/acoes_list.html")
+
+def acoes_favoritas_view(request):
+    ...
+    return render(request, "core/pages/acoes_favoritas.html")
+
+def acoes_compradas_view(request):
+    ...
+    return render(request, "core/pages/acoes_compradas.html")
+
+def acao_detail_view(request):
+    ...
+    return render(request, "core/pages/acao_detail.html")
+
+def perfil_view(request):
+    ...
+    return render(request, "core/pages/perfil.html")
+
+def home_view(request):
+    ...
+    return render(request, "core/pages/home.html")
