5f0fb59e411578024c4df1b173ea63488cf6199e
Matheus Maia <matheusmaia535@gmail.com>
2025-06-22 20:30:34 -0300

    remove arquivos não utilizados e código obsoleto

--- users/forms/login_form.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Nome de Usuário")
-    password = forms.CharField(widget=forms.PasswordInput, label="Senha")
